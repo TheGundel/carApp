74bd31a84fec5f9d68698b7ebb13ed1060c730af
sara <saranordberg@me.com>
2019-04-03 14:16:27 +0200

    Added a timer

---
+++ car/StopWatch.swift
@@ -0,0 +1,36 @@
+//
+//  StopWatch.swift
+//  car
+//
+//  Created by Sara Nordberg on 03/04/2019.
+//  Copyright © 2019 Sara Nordberg. All rights reserved.
+//
+
+import Foundation
+
+class StopWatch {
+    
+    private var startTime: NSDate?
+    
+    var elapsedTime: TimeInterval {
+        if let startTime = self.startTime {
+            return -startTime.timeIntervalSinceNow
+        }
+        else {
+            return 0
+        }
+    }
+    
+    var isRunning: Bool {
+        return startTime != nil
+    }
+    
+    func start() {
+        startTime = NSDate()
+    }
+    
+    func stop() {
+        startTime = nil
+    }
+    
+}
